4446745afb302fd44c47a73219ae97f09716e2a1
Atul Kumar <40385822+Atulya1@users.noreply.github.com>
2025-03-08 23:36:43 -0500

    Dev atul (#12)
    * Dev branch created for atul
    
    * Adding gitignore file
    
    * Delete .idea directory
    
    * Pushing pipeline
    
    * pipeline modified to add logging
    
    * Documentation and query added
    
    * es_query added
    
    * pipeline improved
    
    * pipeline improved
    
    * pipeline improved
    
    * Pipeline fixed
    
    * pipeline fixed
    
    ---------
    
    Co-authored-by: Atul Kumar <atulkumar@macbookpro.mynetworksettings.com>
    Co-authored-by: Atul Kumar <atulkumar@Atuls-MacBook-Pro.local>

--- Data-Pipeline/scripts/data_indexing_elasticsearch.py
+++ Data-Pipeline/scripts/data_indexing_elasticsearch.py
@@ -5,7 +5,6 @@ Creates an Elasticsearch index with a custom schema/analysis,
 then indexes tweet documents into that index.
 
 Usage:
-
     1) python data_index_elasticsearch_tests.py
        (For a standalone test, it will create the index and optionally index some example docs.)
     2) In your DAG or another script, call:
@@ -13,10 +12,6 @@ Usage:
        create_index("tweets_ukraine", stopwords_list)
        index_tweets("tweets_ukraine", tweets_dict)
 
-Requires:
-    - elasticsearch7 library (pip install elasticsearch7)
-    - logging_config.get_logger for logging
-    - nltk stopwords if you use them in the "english_stop" filter
 """
 import hashlib
 from elasticsearch7 import Elasticsearch
@@ -26,8 +21,6 @@ from data_schema import get_es_mappings
 
 import nltk
 from nltk.corpus import stopwords
-nltk.download('stopwords')
-nltk_stopwords = stopwords.words('english')
 
 logger = get_logger("data_indexing_elasticsearch.log", logger_name=__name__)
 
@@ -42,7 +35,9 @@ def get_es_client(host="http://localhost:9200", timeout=30):
         logger.warning(f"Failed to connect to Elasticsearch at {host}")
     return es
 
-def create_index(es_client, index_name, stopwords):
+
+def create_index(es_client, index_name):
+    nltk_stopwords = stopwords.words('english')
     """
     Creates an index in Elasticsearch with custom analyzers + mappings.
     If the index already exists, logs the event, returns False, and does nothing.
@@ -61,7 +56,7 @@ def create_index(es_client, index_name, stopwords):
                 "filter": {
                     "english_stop": {
                         "type": "stop",
-                        "stopwords": stopwords
+                        "stopwords": nltk_stopwords
                     },
                 },
                 "analyzer": {
@@ -84,8 +79,10 @@ def create_index(es_client, index_name, stopwords):
     logger.info(f"Index '{index_name}' has been created with custom settings.")
     return True
 
+
 def generate_id_from_text(text):
     return hashlib.md5(text.encode('utf-8')).hexdigest()
+
 
 def index_tweets(es_client, index_name, tweets_dict):
     """
@@ -107,24 +104,30 @@ def index_tweets(es_client, index_name, tweets_dict):
 
     logger.info(f"Finished indexing. Total successful: {count}")
 
+
 def index_elasticsearch(tweets, index_name):
-    es_client = get_es_client(host="http://localhost:9200", timeout=30)
+    if create_index(get_es_client(), index_name):
 
-    if create_index(es_client, index_name, nltk_stopwords):
-
-        index_tweets(es_client, index_name, tweets)
+        index_tweets(get_es_client(), index_name, tweets)
         logger.info(f"Index '{index_name}'. Indexing Completed.")
 
     else:
         logger.info(f"Index '{index_name}' already exists. Skipping indexing.")
 
 
+def get_index_name(version):
+    version_map = {
+        1: "tweets_ukraine_version_1",
+        2: "tweets_ukraine_version_2",
+        3: "tweets_ukraine_version_3",
+    }
+    return version_map.get(version, False)
+
+
 def main():
+    tweets = parse_folder("../data/version_3")
+    index_elasticsearch(tweets, get_index_name(3))
 
-    tweets = parse_folder("../data/version_1")
-    index_name = "tweets_ukraine_version_1"
-
-    index_elasticsearch(tweets, index_name)
 
 if __name__ == "__main__":
     main()
